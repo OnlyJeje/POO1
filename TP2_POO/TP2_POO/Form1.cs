6a0cd1869557510c2aada4d2f98c8327c3daa280
OnlyJeje <jeremy.bailly@epitech.eu>
2015-11-24 17:45:07 -0500

    Lecture de fichier binaire, text + modification affichage
    A faire : Ajout/Suppression d'item dans inventory

--- TP2_POO/TP2_POO/Form1.cs
+++ TP2_POO/TP2_POO/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,12 @@ namespace TP2_POO {
 
         string cmd = "";
         double solde = 0.00d;
+        double[] inventory = new double[14];
+        double[] price = new double[14];
+        string[] name = new string[14];
+        bool display = true;
+        int iDisplay = 0;
+        /* Mode de l'interface */
         enum mode {
             user,
             technician
@@ -149,17 +156,70 @@ namespace TP2_POO {
         public void Display(string str1, string str2) {
             label1.Text = str1;
             label2.Text = str2;
+            /* RAZ timer pour cohérence affichage */
+            timer1.Stop();
+            timer1.Start();
         }
 
         public Form1() {
             InitializeComponent();
             label1.Text = "SOLDE";
             label2.Text = solde.ToString();
+            ReadBinaryFile("data.bin", inventory);
+            ReadBinaryFile("prix.bin", price);
+            ReadFile("nom.txt");
+            timer1.Start();
+            //MessageBox.Show(name[13]);
+            //MessageBox.Show(inventory[5].ToString());
         }
 
+        /* Ecrit dans le fichier filename les données du tableau data */
+        public void WriteBinaryFile(string filename, double[] data) {
+            using (FileStream fileStream = new FileStream(filename,  FileMode.OpenOrCreate))
+            using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+            {
+                        for (int i = 0; i < data.Length; i++) {
+                            binaryWriter.Write(data[i]);
+                        }
+            }
+        }
+
+        /* Lit les données dans le fichier filename et les stocks dans le tableau data */
+        public void ReadBinaryFile(string filename, double[] data) {
+            using (BinaryReader b = new BinaryReader(File.Open(filename, FileMode.OpenOrCreate))) {
+                int pos = 0;
+                int i = 0;
+                int length = (int)b.BaseStream.Length;
+                while (pos < length) {
+                    double v = b.ReadDouble();
+                    data[i++] = v;
+                    pos = pos + sizeof(double);
+                }
+            }
+        }
+
+        /*Lit le fichier filename et le stock dan le tableau name*/
+        public void ReadFile(string filename) {
+            if (File.Exists(filename))
+                name = File.ReadAllLines(filename);
+            else
+                MessageBox.Show("Le fichier nom.txt n'existe pas");
+            }
+
+        /* Gestion de l'affichage */
         private void timer1_Tick(object sender, EventArgs e) {
-            Display("SOLDE", solde.ToString());
-            timer1.Stop();
+            if (iDisplay > 13)
+                iDisplay = 0;
+            if (display == true) { 
+                Display("SOLDE", solde.ToString());
+                display = false;
+            }
+            else if (display == false) {
+                if (inventory[iDisplay] != 0)
+                    Display("" + name[iDisplay] + " " , "" + inventory[iDisplay] + " " + price[iDisplay] + "");
+                display = true;
+                iDisplay++;
+            }
         }
     }
 }
