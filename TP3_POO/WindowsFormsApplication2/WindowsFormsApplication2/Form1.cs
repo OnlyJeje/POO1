418653733604b3d4a8345f85ba72c1b3b5b9df3b
dechev_a <dechev_a@epitech.eu>
2015-12-16 14:03:12 -0500

    Finalisation
    Finalisation des commentaires

--- TP3_POO/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
+++ TP3_POO/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -36,34 +36,27 @@ namespace WindowsFormsApplication2 {
         }
         private void Form1_Load(object sender, EventArgs e) {
 
-            /*Le code de base repris du prof pour l'initialisation du tableau*/
-            graph = new ZedGraphControl();      // Créer la composante graphique
-            graph.Parent = panel;               // Placer la composante sur le panel de la fiche
+            /*Le code qui suit est repris de l'exemple pour l'initialisation du graph*/
+            graph = new ZedGraphControl();     
+            graph.Parent = panel;               
 
             // Ajuster la taille de la composante en fonction de la fiche
             graph.Width = panel.Width - 4;
             graph.Height = panel.Height - 4;
 
-            pane = graph.GraphPane;                 // Créer l'objet graphique
-            pane.XAxis.MajorGrid.IsVisible = true;  // Demander l'affichage de grilles
+            //Afficher la grille, nommer les axes et nommer provisoirement le graphique
+            pane = graph.GraphPane;               
+            pane.XAxis.MajorGrid.IsVisible = true;  
             pane.YAxis.MajorGrid.IsVisible = true;
-            pane.XAxis.Title.Text = "Abcisse (X)";  // Identifier les axes
-            pane.YAxis.Title.Text = "Ordonnée (Y)";
+            pane.XAxis.Title.Text = "Volume (V) en cm3 ";
+            pane.YAxis.Title.Text = "Pression P (Kg/cm2)";
             pane.Title.Text = "Le titre du graphique";
 
-            // Mettre une petite couleur de fond pour faire joli
             pane.Chart.Fill = new Fill(Color.White, Color.LightBlue, 45.0f);
         }
 
         private void do_work() {
-            /*Ici devrait être une fonction créant une liste à partir du fichier */
-
             // On insère les valeurs sur notre graphique ==> créer une "courbe".
-            //      La courbe est rouge avec un cercle à la position de chacune des valeurs.
-            /*Le truc à retenir c'est le AddCurve qui est intéressant mais non fonctionnel ici,
-             * Il faudra simplement le réutiliser avec nos points (ceux résultant de la médiation
-             entre les coordonnées, la ligne du millieu sur le schèma) et ps: les couleurs ne sont pas
-             * bonnes entre le commentaire et le code ;)*/
             ymodif = coordY.ToArray();
 
             for (int i = 0; i < ymodif.Length; i++) {
@@ -74,25 +67,22 @@ namespace WindowsFormsApplication2 {
                 courbe.Symbol.Fill = new Fill(Color.Red);
 
             // On ajuste les axes en fonctions des étendues de X et Y
-            /*Mettre notre échelle*/
             pane.XAxis.Scale.Min = coordX.Min();
             pane.XAxis.Scale.Max = coordX.Max();
             pane.YAxis.Scale.Min = 0;
             pane.YAxis.Scale.Max = 60;
 
-            // On donne des titres aux axes et au graphique
-            /*Mettre nos noms*/
-            pane.XAxis.Title.Text = "Volume (V) en cm3 ";
-            pane.YAxis.Title.Text = "Pression P (Kg/cm2)";
+            //Attribution du bon titre pour le graphique           
             pane.Title.Text = "Graphique de la fonction Y = " + A.ToString() + ".X + " + B.ToString() + "\nCoefficient R = " + R.ToString();
 
             // On finalise le graphique
-            /*Ouais ok, cool story*/
-            pane.Legend.IsVisible = false;  // Pas besoin de légende
-            graph.AxisChange();             // Pour forcer un réétalonnage des axes
-            graph.Invalidate();             // Pour redessiner le graphique
+            pane.Legend.IsVisible = false;
+            graph.AxisChange();           
+            graph.Invalidate();           
         }
 
+        //Ici l'utilisateur choisit le fichier qu'il souhaite charger, ce fichier est ouvert, son contenu est disposer dans deux tableaux
+        //Ensuite les opérations sont effectuer pour calculer la courbe
         private void LoadFile_Click(object sender, EventArgs e) {
 
 
@@ -125,7 +115,7 @@ namespace WindowsFormsApplication2 {
             R = coeffCorrel(x, y);
             do_work();
         }
-
+        //Pour les fonctions calcB et calcA ce sont toutes les opérations pour calculer les coordonnées
         double calcB(double[] tabX, double[] tabY) {
             double res = 0; ;
             res = ((sumY * sumSquareX) - (sumX * sumXY)) / ((tabX.Length * sumSquareX) - (Math.Pow(sumX,2)));
@@ -152,7 +142,7 @@ namespace WindowsFormsApplication2 {
             return res;
 
             }
-
+        //Calcul du coefficient de correlation afin de lisser la courbe
         double coeffCorrel(double[] tabX, double[] tabY) {
             double res = 0;
             double calc1 = 0;
